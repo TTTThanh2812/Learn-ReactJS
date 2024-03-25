a46ce1b1248a8a40e0940c0cdd1d43c413fe95c4
TTTThanh2812 <thanh.truongthithanh@drcomgroup.com>
2024-03-25 10:35:38 +0700

    next

--- Learn_ReactJS/src/compoments/HomeCards.jsx
+++ Learn_ReactJS/src/compoments/HomeCards.jsx
@@ -5,7 +5,6 @@ import Card from "./Card";
 const HomeCards = () => {
   return (
     <>
-      {/* <!-- Developers and Employers --> */}
       <section className="py-4">
         <div className="container-xl lg:container m-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
---
+++ Learn_ReactJS/src/compoments/ViewAllJobs.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const ViewAllJobs = () => {
+  return (
+    <>
+      <section className="m-auto max-w-lg my-10 px-6">
+        <a
+          href="/jobs"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">
+          View All Jobs
+        </a>
+      </section>
+    </>
+  );
+};
+
+export default ViewAllJobs;
